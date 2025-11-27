2e25a5c3b6ef6bb0b3f0884bd7509ac6ef9709e8
Matt Davis <mattdavis90@googlemail.com>
2025-11-27 20:55:29 +0000

    latest immich API

--- update_openapi.sh
+++ update_openapi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=2.0.1
+VERSION=2.3.1
 
 pushd client
 curl https://raw.githubusercontent.com/immich-app/immich/v${VERSION}/open-api/immich-openapi-specs.json -o immich-openapi-specs.json
